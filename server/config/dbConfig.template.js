3e102b5b2e9d5753206b964b14b5d95973f9ad99
Dong Yang <doyang@e1z2r5p36.42.us.org>
2019-10-08 21:54:25 -0700

    backend setup

---
+++ server/config/dbConfig.template.js
@@ -0,0 +1,5 @@
+module.exports = {
+    url: '', //for mlab
+    user: '',
+    pwd: ''
+}
--- server/server.js
+++ server/server.js
@@ -1,7 +1,78 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+const fs = require('fs');
+
+// DB Config
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+
+//session
+const session = require("express-session");
+const sessionConfig = require("./config/serverConfig.json").session;
+const MongoStore = require('connect-mongo')(session);
+
+// Connect to MongoDB
+mongoose.connect(
+    db,
+	{ 
+		useNewUrlParser: true,
+		useUnifiedTopology: true 
+	}
+)
+.then(() => console.log("MongoDB successfully connected"))
+.catch(err => console.log(err));
+const connectedDb = mongoose.connection;
+
+// Define Socket.io
+const http = require('http').Server(app);
+const io = require('socket.io')(http, { pingTimeout: 60000 });
+const sharedsession = require('express-socket.io-session');
+
+// Router
+/* const rooms = require(''); */
+
+//session settings
+//session secret is stored in serverConfig.json which is gitignored for version control.
+const sessionInfo = {
+    secret: sessionConfig.secret, // secret key to verify session
+    resave: false,
+	saveUninitialized: true,
+	store: new MongoStore({ mongooseConnection: connectedDb })
+}
+
+app.use(session(sessionInfo));
+
+// set socket
+io.use(sharedsession(session(sessionInfo), {
+    autoSave:true
+}));
+
+let people = [];
+io.sockets.on('connection', (socket) => {
+	// change to username_related chatroom using mysql db
+    socket.on('disconnect', () => {
+        /* console.log("Disconnected from the download server"); */
+	})
+	people[socket.handshake.session.logged_user] = socket.id;
+	socket.handshake.session.save();
+	
+	socket.on('ping', msg => {
+		alert(msg);
+	});
+});
+global.people = people;
+
+//Bodyparser middleware 
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use(bodyParser.json());
+
+
 const port = process.env.PORT || 5000;
-
-
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
